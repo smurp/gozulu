cbda0cdaa9ef2a361ba18a94f26e949f67558222
Shawn Murphy <smurp@smurp.com>
2025-03-06 14:24:15 -0800

    make release.sh take a commit message

--- release.sh
+++ release.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-git checkout live && git merge main -m "release" && git push && git checkout main
+# Check if a commit message was provided
+if [ $# -eq 0 ]; then
+  # Default commit message if none provided
+  COMMIT_MSG="release"
+else
+  # Use all arguments as the commit message
+  COMMIT_MSG="$*"
+fi
+
+# Output what we're doing
+echo "Releasing with commit message: \"$COMMIT_MSG\""
+
+# Perform the release
+git checkout live && git merge main -m "$COMMIT_MSG" && git push && git checkout main
